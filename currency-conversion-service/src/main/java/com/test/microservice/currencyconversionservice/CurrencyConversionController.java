4f5caecb2aca83dd0d706fd842a296632a384146
Shubham Nagre <shubhamnagre14@gmail.com>
2022-05-04 19:21:10 +0530

    FEIGN implementation for rest controller in currency conversion talking to currency exchange ms

--- currency-conversion-service/src/main/java/com/test/microservice/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/test/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 import org.bouncycastle.jcajce.provider.digest.GOST3411.HashMac;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
+	@Autowired
+	CurrencyExchangeProxy proxy;
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
@@ -27,6 +31,19 @@ public class CurrencyConversionController {
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 		
 //		return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "8100");
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+	}
+	
+	// FEIGN implementation
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign (
+			@PathVariable String from, 
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		// Getting details from Currency Exchange Controller API
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
 		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
 	}
 }
