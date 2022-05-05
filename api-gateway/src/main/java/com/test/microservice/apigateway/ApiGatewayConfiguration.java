8e3e9b0112976b05256064b00c5f45b57f8dd3f0
Shubham Nagre <shubhamnagre14@gmail.com>
2022-05-05 18:21:51 +0530

    Implementing Spring Cloud Gateway, Routing and Global FilteringAPIs

---
+++ api-gateway/src/main/java/com/test/microservice/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,50 @@
+package com.test.microservice.apigateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//		Function<PredicateSpec, Buildable<Route>> routeFunction = 
+//				p -> p.path("/get")
+//				.filters(f -> f
+//						.addRequestHeader("MyHeader", "MyURI")
+//				        .addRequestParameter("Param", "MyValue"))
+//				.uri("http://httpbin.org:80");
+				
+//		return builder.routes().route(routeFunction ).build();
+		
+		
+		// CUSTOM FILTERS
+		// Inline (Short code) (basic currency-conversion is disabled in my code; try with feign)
+		return builder.routes()
+				.route(p -> p.path("/get")
+						.filters(f -> f
+								.addRequestHeader("MyHeader", "MyURI")
+								.addRequestParameter("Param", "MyValue"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-new/**")		// Custom URI to rewrite value with required one
+				        .filters(f -> f.rewritePath(
+				        		"/currency-conversion-new/(?<segment>.*)", // (1,2) : 1 - rewriting to, 2 -> rewriting from 
+				        		"/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion"))
+				.build();
+				
+	}
+}
